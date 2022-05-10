d888d3138a0040725958f4ac15e83719b8a8e5b4
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-05-11 01:41:03 +0400

    Connected to remote database

--- controller/task.js
+++ controller/task.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const Router = express.Router()
 const mongoose = require("mongoose")
+require("dotenv").config()
 
 const Tasks = require("../model/task")
 
 try {
-    mongoose.connect("mongodb://localhost:27017/Tasks")
+    mongoose.connect(process.env.MONGO_URI)
     console.log("Connection Successful")
 } catch (error) {
     if(error) throw error
